4f532ab4cb195c55b3eb3ee2d426ce0f77fd4998
Wok <woctezuma@users.noreply.github.com>
2022-11-13 11:18:37 +0100

    Fix bug: use ALL words for the chunked pattern generation

--- src/cache_utils.py
+++ src/cache_utils.py
@@ -6,13 +6,14 @@ from src.pattern_utils import generate_pattern_dict
 
 
 def pre_compute_patterns(all_dictionary):
+    all_words = set(all_dictionary)
     num_chunks = get_num_chunks(all_dictionary)
     for chunk_no, dictionary_chunk in enumerate(chunks(all_dictionary), start=1):
         fname = get_pattern_dict_fname(chunk_no)
         print(f"[{chunk_no}/{num_chunks}] Processing {fname}")
         if not os.path.exists(fname):
             # Calculate the pattern_dict and cache it
-            pattern_dict = generate_pattern_dict(dictionary_chunk)
+            pattern_dict = generate_pattern_dict(dictionary_chunk, all_words)
             save_pattern_dict(pattern_dict, chunk_no)
 
     return num_chunks
