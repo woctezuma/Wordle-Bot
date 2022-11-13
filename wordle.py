3ff23ebe76a888e2ab13af2b5b644436d5c95d4b
Wok <woctezuma@users.noreply.github.com>
2022-11-13 11:51:59 +0100

    Add a command-line interface (argparse)

--- wordle.py
+++ wordle.py
@@ -1,3 +1,5 @@
+import argparse
+
 from tqdm import tqdm
 
 from src.cache_utils import pre_compute_patterns
@@ -6,24 +8,43 @@ from src.entropy_utils import make_a_guess
 from src.utils import update_possible_words
 
 N_GUESSES = 6
-FIRST_GUESS = "tares"
 DATA_FOLDER = "data/"
-GAME_FOLDER = "wordle/"
-DICT_FILE_ALL = f"{DATA_FOLDER}{GAME_FOLDER}guesses.txt"
-DICT_FILE = f"{DATA_FOLDER}{GAME_FOLDER}solutions.txt"
+GAME_NAMES = ["wordle", "dungleon"]
+DICT_FILE_ALL = "guesses.txt"
+DICT_FILE = "solutions.txt"
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--game-name",
+        type=str,
+        choices=GAME_NAMES,
+        default="wordle",
+        help="Game name",
+    )
+    parser.add_argument(
+        "--first-guess",
+        type=str,
+        default=None,
+        help="Pre-computed first guess",
+    )
+    args = parser.parse_args()
+
+    game_folder = f"{args.game_name}/"
+    fname_all_words = f"{DATA_FOLDER}{game_folder}{DICT_FILE_ALL}"
+    fname_solutions = f"{DATA_FOLDER}{game_folder}{DICT_FILE}"
+
     # load all 5-letter-words for making patterns
-    all_dictionary = load_word_dictionary(DICT_FILE_ALL)
+    all_dictionary = load_word_dictionary(fname_all_words)
 
     num_chunks = pre_compute_patterns(all_dictionary)
 
     # Load 2315 words for solutions
-    dictionary = load_word_dictionary(DICT_FILE)
+    dictionary = load_word_dictionary(fname_solutions)
 
     # Simulate games
-    precomputed_first_guess = FIRST_GUESS
+    precomputed_first_guess = args.first_guess
 
     for word_to_guess in tqdm(dictionary):
 
